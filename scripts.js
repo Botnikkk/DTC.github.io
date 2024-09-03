76a2f34b229378e47e66cf40f0e849cc1b41f45a
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2024-09-03 09:25:52 +0530

    login and signup page setup

--- scripts.js
+++ scripts.js
@@ -26,3 +26,34 @@ function handleSignup(event) {
     alert(`Signup successful! Welcome, ${name}`);
     window.location.href = '#login'; // Redirect to the login form
 }
+
+
+
+let signUp = document.querySelector('.signUp')
+let login = document.querySelector('.login')
+
+let loginContainer = document.querySelector('.container-login')
+let signupContainer = document.querySelector('.container-signup')
+
+signUp.addEventListener('click', function(){
+
+    console.log('signup')
+
+
+    loginContainer.style.transform = "rotateY(180deg)"
+    signupContainer.style.transform = "rotateY(0deg)"
+
+    loginContainer.style.zIndex = "1"
+    signupContainer.style.zIndex = "2"
+})
+
+login.addEventListener('click', function(){
+
+    console.log('login')
+
+    signupContainer.style.transform = "rotateY(180deg)"
+    loginContainer.style.transform = "rotateY(0deg)"
+
+    signupContainer.style.zIndex = "1"
+    loginContainer.style.zIndex = "2"
+})
